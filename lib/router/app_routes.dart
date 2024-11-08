2b6cc9e24aff3de5738e32109b20520a506b7e0d
lorena <lcalorg3006@g.educaand.es>
2024-11-08 09:53:19 +0100

    parte3

--- lib/router/app_routes.dart
+++ lib/router/app_routes.dart
@@ -1,10 +1,17 @@
 import 'package:fl_components/screens/screens.dart';
 import 'package:flutter/material.dart';
-
+import 'package:fl_components/models/models.dart';
 
 
 class AppRoutes {
   static const initialRoute = 'home';
+  static final Menuoptions = <MenuOption>[
+    MenuOption(route: 'home', icon: Icons.home_max_sharp, name: "Home Screen", screen:const  HomeScreen()),
+    MenuOption(route: 'listview1', icon: Icons.list_alt, name: "Listview tipo 1", screen:const  Listview1Screen()),
+    MenuOption(route: 'listview2', icon: Icons.list, name: "Listview tipo 2", screen:const  Listview2Screen()),
+    MenuOption(route: 'alert', icon: Icons.add_alert_outlined, name: "Alertas", screen:const  AlertScreen()),
+    MenuOption(route: 'card', icon: Icons.credit_card, name: "Tarjetas - Cards", screen:const  CardScreen()),
+  ];
 
   static Map<String, Widget Function(BuildContext)> routes = {
         "listview1": (BuildContext context ) => const Listview1Screen() ,
