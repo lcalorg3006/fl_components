b923186459d27f60a860cb514e565669cad355fb
lorena <lcalorg3006@g.educaand.es>
2024-11-25 10:58:19 +0100

    AlertDialog

--- lib/screens/alert_screen.dart
+++ lib/screens/alert_screen.dart
@@ -3,12 +3,35 @@ import 'package:flutter/material.dart';
 class AlertScreen extends StatelessWidget {
    
   const AlertScreen({Key? key}) : super(key: key);
-  
+  void displayDialog(BuildContext context){
+    showDialog(
+      barrierDismissible: true,// fuera de la alerta se sale
+      context: context,
+       builder: (context){
+        return  AlertDialog(
+          title: Text('Alerta'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text("Este es el contenido de la alerta"),
+              SizedBox(height: 10),//espacio
+              FlutterLogo(size: 100)
+            ],
+          ),
+        );
+       });
+    }
+
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
-      body: const Center(
-         child: Text('AlertScreen'),
+      body: Center(
+         child: ElevatedButton(
+          onPressed:() => displayDialog(context),
+           child: Padding(
+             padding: const EdgeInsets.all(20.0),
+             child: const Text('mostrar alerta', style: TextStyle(fontSize: 20),),
+           ))
       ),
       // boton flotante para cerrar
       floatingActionButton: FloatingActionButton(
@@ -18,3 +41,4 @@ class AlertScreen extends StatelessWidget {
     );
   }
 }
+
--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -2,33 +2,39 @@ import 'package:flutter/material.dart';
 
 class AppTheme {
   static const Color primary = Colors.green;
-  static final ThemeData lightTheme =  ThemeData.light().copyWith( // sobre escribe las propiedades
-           
-           //color primario
-           primaryColor: primary,
-        //tema al appbar
+  static final ThemeData lightTheme = ThemeData.light().copyWith(
+      // sobre escribe las propiedades
 
-        //color primario
-        appBarTheme: const AppBarTheme(
-        color: primary,
-        titleTextStyle: TextStyle(color: Colors.white, fontSize: 24),
-        iconTheme: IconThemeData(color: Colors.white)
-        ),
-        // para que los iconos salgan naranja tambien
+      //color primario
+      primaryColor: primary,
+      //tema al appbar
+
+      //color primario
+      appBarTheme: const AppBarTheme(
+          color: primary,
+          titleTextStyle: TextStyle(color: Colors.white, fontSize: 24),
+          iconTheme: IconThemeData(color: Colors.white)),
+      // para que los iconos salgan naranja tambien
       listTileTheme: const ListTileThemeData(
         iconColor: primary,
       ),
       // text button theme
       textButtonTheme: TextButtonThemeData(
-      style: TextButton.styleFrom(foregroundColor: primary),
+        style: TextButton.styleFrom(foregroundColor: primary),
       ),
-
       floatingActionButtonTheme: const FloatingActionButtonThemeData(
         backgroundColor: primary,
-        shape: StadiumBorder(),//botton redonde
-        elevation: 5,//sombreado
-        foregroundColor: Colors.white,//texto en blanco
-      )
-      );
-       //theme:themeData.dark   // modo oscuro
+        shape: StadiumBorder(), //botton redonde
+        elevation: 5, //sombreado
+        foregroundColor: Colors.white, //texto en blanco
+      ),
+      //ElevatedButtons
+      elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+             backgroundColor: primary,
+             foregroundColor: Colors.white,
+             shape: const StadiumBorder(),
+             elevation: 5,
+      )));
+  //theme:themeData.dark   // modo oscuro
 }
